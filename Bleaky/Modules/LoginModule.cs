6ca19acf113bd2196c3e9632f9671c8ecdbcb4d2
Matt Jones <mattyjones@gmail.com>
2013-07-30 20:53:12 +0100

    Updates bootstrap and starts login.
    Looking swish.

---
+++ Bleaky/Modules/LoginModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace Bleaky.Modules
+{
+    public class LoginModule : NancyModule
+    {
+        public LoginModule() : base("/Login")
+        {
+            Get["/"] = Login;
+        }
+
+        dynamic Login(dynamic parameters)
+        {
+            return View["/Index.cshtml"];
+        }
+    }
+}
