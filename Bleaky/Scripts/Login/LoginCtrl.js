5bd7d8986e7b00bf8bbbe389899a67e150d6223f
Matt Jones <mattyjones@gmail.com>
2013-08-13 21:35:06 +0100

    Starts angular verification for registration

---
+++ Bleaky/Scripts/Login/LoginCtrl.js
@@ -0,0 +1,9 @@
+﻿var BleakyApp = angular.module('BleakyApp', []);
+
+BleakyApp.controller('LoginCtrl', function ($scope) {
+    $scope.verifyRegistration = function () {
+        console.log($scope.inputEmail);
+        console.log($scope.inputPassword);
+        console.log($scope.inputConfirmPassword);
+    };
+});
