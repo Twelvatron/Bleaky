b17cda51b53e5600e5f83b016ddcb600bdbab338
Matt Jones <mattyjones@gmail.com>
2013-10-06 22:06:07 +0100

    Continues workout wizard

--- Bleaky/Scripts/Workout/WorkoutCtrl.js
+++ Bleaky/Scripts/Workout/WorkoutCtrl.js
@@ -1,7 +1,19 @@
 ﻿var BleakyApp = angular.module('BleakyApp');
 
 BleakyApp.controller('WorkoutCtrl', function ($scope) {
+
+    $scope.days = [1, 2, 3, 4, 5, 6, 7];
+    $scope.inputWorkoutFrequency = 1;
+
     $scope.AddWorkout = function () {
-        $scope.showAddWorkout = true;
+        var addWorkoutWizDiv = $('#blAddWorkoutWiz');
+        addWorkoutWizDiv.slideToggle();
+    };
+
+    $scope.AddDays = function () {
+        var addWorkoutDiv = $('.blAddWorkoutDiv');
+        var addDaysDiv = $('.blAddDaysDiv');
+        addWorkoutDiv.slideToggle();
+        addDaysDiv.slideToggle();
     };
 });
