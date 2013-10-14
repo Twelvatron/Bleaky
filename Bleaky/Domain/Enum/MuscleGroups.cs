7c391c8377177d7d4ac59d014eb723c5d552bb1a
Matt Jones <mattyjones@gmail.com>
2013-10-14 22:14:37 +0100

    Continues dev on svgs and enums

---
+++ Bleaky/Domain/Enum/MuscleGroups.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bleaky.Domain.Enum
+{
+    public enum MuscleGroups
+    {
+        Biceps = 1,
+        Shoulders = 2,
+        Chest = 3,
+        Triceps = 4,
+        Abs = 5,
+        Oblique = 6,
+        Forearms = 7,
+        Quads = 8,
+        Calves = 9,
+        LowerBack = 10,
+        Glutes = 11,
+        Groin = 12,
+        UpperBack = 13
+    }
+}
